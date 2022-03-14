c00b743c40a342a8377440d453232f203d2c7326
Jon Giarla <jonathangiarla@gmail.com>
2022-03-14 17:42:29 -0400

    Added equals functionality, including the ability to perform an operation based on the previous computation. Also added the ability to find the exponent of two numbers and to use a decimal point. Next step is to allow an operator to show the anser in order to chain operators together.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Calculator
 A multipurpose calculator
+
+
+Ver 0.7: Added equals functionality, including the ability to perform an operation based on the previous computation. Also added the ability to find the exponent of two numbers and to use a decimal point.  Next step is to allow an operator to show the answer in order to chain operators together.
--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ const numbers = document.getElementsByClassName('number');
 const operator = document.getElementsByClassName('operator');
 const clear = document.getElementById('clear');
 const equals = document.getElementById('equals');
-let firstOperand = 0;
-let secondOperand = 0;
+let firstOperand = '';
+let secondOperand = '';
 let operatorSign = '';
-
+let fullDisplay = false;
 
 function sum(x, y) {
-    console.log(`${x} + ${y} = ${x + y}`)
+    console.log(`${x} + ${y} = ${parseFloat(x) + parseFloat(y)}`)
     console.log(typeof (x + y));
     return (parseFloat(x) + parseFloat(y));
 }
@@ -34,6 +34,10 @@ function quot(x, y) {
     }
 }
 
+function expo(x, y) {
+    return x ** y;
+}
+
 function operation(num1, num2, operand) {
     switch (operand) {
         case '+':
@@ -48,6 +52,9 @@ function operation(num1, num2, operand) {
         case '/':
             return quot(num1, num2);
             break;
+        case 'x^y':
+            return expo(num1, num2);
+            break;
         default:
             return 'ERROR';
     }
@@ -56,19 +63,20 @@ function operation(num1, num2, operand) {
 }
 
 function clearDisplay() {
+    fullDisplay = false;
     display.innerText = '';
-    firstOperand = 0;
-    secondOperand = 0;
+    firstOperand = '';
+    secondOperand = '';
 }
 
 function addToDisplay(num) {
     display.innerText += num;
     console.log(num);
-    firstOperand = display.innerText;
 }
 
 for (let i = 0; i < operator.length; i++) {
     operator[i].onclick = function () {
+        fullDisplay = false;
         operatorSign = operator[i].innerText;
         firstOperand = display.innerText;
         display.innerText = '';
@@ -77,6 +85,10 @@ for (let i = 0; i < operator.length; i++) {
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].onclick = function () {
+        if (fullDisplay == true) {
+            clearDisplay();
+            fullDisplay = false;
+        }
         addToDisplay(numbers[i].innerText)
     };
 }
@@ -87,5 +99,6 @@ equals.onclick = function () {
     secondOperand = display.innerText;
     console.log(firstOperand);
     display.innerText = operation(firstOperand, secondOperand, operatorSign);
+    fullDisplay = true;
     firstOperand = display.innerText;
 }
